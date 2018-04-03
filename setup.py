972d3ab2dd6783b7ae59e04f587bdb57ca4651b7
Jaekyeom Kim <btapiz@gmail.com>
2018-04-03 16:37:21 +0900

    Bump version.

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ class BuildDoom(DistutilsBuild):
         DistutilsBuild.run(self)
 
 setup(name='doom-py',
-      version='0.0.18',
+      version='0.0.19',
       description='Python bindings to ViZDoom',
       url='https://github.com/jaekyeom/doom-py',
       author='OpenAI Community',
